0c2b04d7277ebf4870435993e004614842e86897
Corazon0D <korazon@gmail.com>
2025-09-29 14:49:54 +0300

    TV

--- strings3.py
+++ strings3.py
@@ -1,9 +1,11 @@
 # Неизменяемость строки (immutable не мутирует   не изменяется)
 
-string = 'Езык Python'
+string = 'Теливидение'
 
 ya = 'я'
-res = ya + string[1:]
+#res = ya + string[1:]
+res = string.replace('и', 'е', 1)
 
 print(res)
+
 
