067f609e15637f98517c6f97dc4f8ea2e711039b
Corazon0D <korazon@gmail.com>
2025-09-29 13:23:00 +0300

    for

--- strings1.py
+++ strings1.py
@@ -3,4 +3,8 @@ sting = 'Привет'
 lenth = len(sting) # число элементов итерируемого объекта
 
 print('Строка "' + sting + '" имеет длину:', lenth)
+
+for i in range(len(sting))
+    print(sting[i])
+
 # print(sting[3])
