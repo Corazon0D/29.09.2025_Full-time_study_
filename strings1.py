99d228d68005af65fc83d2c8f12e906cbfd476ab
Corazon0D <korazon@gmail.com>
2025-09-29 14:22:10 +0300

    Razvorot

--- strings1.py
+++ strings1.py
@@ -1,8 +1,8 @@
 # Срезы (s, s, s)
-
+# срезы [start : stop -1 : step]
 
 string = input('Введите любую строку: ')
 length = len(string) # число элементов итерируемого объекта
 
-print(string[5 : 8])
+print(string[::-1]) # Разворот
 # Вентилятор -> лят
