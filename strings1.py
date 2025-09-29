cc3c8a3828a91b3aabbea7589ccd84c1ee396602
Corazon0D <korazon@gmail.com>
2025-09-29 13:49:38 +0300

    zad na pered

--- strings1.py
+++ strings1.py
@@ -1,9 +1,11 @@
 #        012345
-sting = input('Введите оюбую строку: ')
-lenth = len(sting) # число элементов итерируемого объекта
+sting = input('Введите любую строку: ')
+length = len(sting) # число элементов итерируемого объекта
 
-print('Строка "' + sting + '" имеет длину:', lenth)
+print('Строка "' + sting + '" имеет длину:', length)
 
-symb = sting[lenth -1]
-print('Последний символ:', symb)
+symb = sting[-1]# sting[lenth -1]
+
+for index in range(-1, -(length + 1), -1):
+    print(sting[index])
 # print(sting[3]).
